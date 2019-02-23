48a19c09b0b81760c795089b7b6aed6646cf07f4
Hakan Karabulut <hakan.karabulut@ugurbilgi.net>
2019-02-23 17:21:22 +0300

    Hata özelleştirme

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -46,7 +46,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+            try{
+                var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
                 return Unauthorized();
@@ -76,6 +77,11 @@ namespace DatingApp.API.Controllers
             return Ok(new{
                 token = tokenHandler.WriteToken(token)
             });
+            }
+            catch
+            {
+                return StatusCode(500,"An error occoured!");
+            } 
         }
     }
 }
